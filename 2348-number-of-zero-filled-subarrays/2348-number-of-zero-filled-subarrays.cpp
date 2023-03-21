66baca4721384705781693f30c53362b645a3c65
Harsh Mishra <104376126+harshgeek3000@users.noreply.github.com>
2023-03-21 22:16:57 +0530

    Time: 173 ms (61.99%), Space: 107.6 MB (54.45%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
@@ -5,22 +5,20 @@ public:
         long long ct=0;
         int i;
         long long ansSubarr=0;
-            //2+(2*ct-2)*2;//calculated when zeroes are found continuously//we can use this formula with some minor changes in code
-        
-        
+
+
         for(i=0;i<nums.size();i++){
             if(nums[i]==0){
                 ct++;
-               
             }
             else if(nums[i]!=0){
                 ansSubarr += ct * (ct+1) / 2;
                 ct=0;
             }
-           
-            
+ 
         }
         ansSubarr += (ct * (ct+1) / 2);
+        
         return ansSubarr;
     }
 };
