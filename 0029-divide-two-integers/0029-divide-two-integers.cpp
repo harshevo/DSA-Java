0ee36e773ff393a9eefe2ab9f8b1c4106108d921
Harsh Mishra <104376126+harshevo@users.noreply.github.com>
2023-03-25 19:32:52 +0530

    Time: 3 ms (61.68%), Space: 5.9 MB (64.73%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -1,11 +1,32 @@
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        if(dividend<=(-2147483648)){
-            if(divisor==-1){
-                return 2147483647;
-            }
+        // if(dividend<=(-2147483648)){
+        //     if(divisor==-1){
+        //         return 2147483647;
+        //     }
+        // }
+        // return dividend/divisor;
+        
+        
+        	
+        if(dividend == divisor)
+            return 1;
+        bool isPositive = (dividend<0 == divisor<0);    
+        unsigned int a = abs(dividend);
+        unsigned int b = abs(divisor);
+        unsigned int ans = 0;
+        while(a >= b){  
+            short q = 0;
+            while(a > (b<<(q+1)))
+                q++;
+            ans += (1<<q);  
+            a = a - (b<<q);  
         }
-        return dividend/divisor;
+        if(ans == (1<<31) and isPositive)   
+            return INT_MAX;
+        return isPositive ? ans : -ans;
+    
+        
     }
 };
