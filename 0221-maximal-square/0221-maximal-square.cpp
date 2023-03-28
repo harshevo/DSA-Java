fe549d8c73147987bbef3ca3a0401cd3d37cc2e4
Harsh Mishra <104376126+harshevo@users.noreply.github.com>
2023-03-28 23:41:58 +0530

    Time: 90 ms (66.62%), Space: 21.1 MB (22.52%) - LeetHub

---
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int h = matrix.size();
+        if(h == 0 || matrix[0].empty()){
+            return 0;
+        }
+        int w = matrix[0].size();
+        int answer =0;
+        
+        vector<vector<int>> dp(h,vector<int>(w));//defautl value =0
+        
+        for(int row=0;row<h;++row){
+            for(int col=0;col<w;++col){
+                if(matrix[row][col]=='1'){
+                    dp[row][col]=1;
+                    if(row>0 && col>0){
+                        dp[row][col] += min({dp[row-1][col],dp[row][col-1],dp[row-1][col-1]});
+                    }
+                    answer = max(answer,dp[row][col]);
+                }
+            }
+            
+        }
+        return answer*answer;
+        
+    }
+};
