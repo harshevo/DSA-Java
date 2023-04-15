777764ec8c12d3c4d8645cc56d6cb546d6d753de
Harsh Mishra <104376126+harshevo@users.noreply.github.com>
2023-04-15 15:22:17 +0530

    Time: 267 ms (77.49%), Space: 19.7 MB (19.90%) - LeetHub

--- 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
@@ -2,6 +2,9 @@ class Solution {
 public:
     int length;
     
+    //with recursion TLE
+    //with recursion with memo : accepted
+    
     int dp[1001][2001];
     
     int solve(int i,vector<vector<int>>& piles,int k){
@@ -11,21 +14,25 @@ public:
         }
         
         if(dp[i][k] != -1){
-            return dp[i][k];
+            return dp[i][k]; 
         }
         
-        int notTaken = solve(i+1,piles,k);
+        int notTaken = solve(i+1,piles,k); //if the pile is not taken then we'll move to next pile
         
-        int taken = 0;
+        int taken = 0; 
         
         int sum = 0;
         
         
         for(int j=0; j < min((int)piles[i].size(),k); j++){
             
-            sum += piles[i][j];
+            sum += piles[i][j]; //iterate through the pile and add the value to the sum
           
-            taken = max(taken , sum + solve(i+1,piles,k-(j+1)));
+            taken = max(taken , sum + solve(i+1,piles,k-(j+1))); //set the value of taken with the max value
+             
+            /*for example , the value of k is 2 , to get the max value we need to iterate through the pile and add the coin and decrease
+                the value of k , as it shows that coin is taken 
+            */
                 
         }
         
@@ -37,7 +44,7 @@ public:
        
        length = piles.size();
         
-        memset(dp,-1,sizeof(dp));
+        memset(dp,-1,sizeof(dp));//set the value of dp elements to -1
         
        return solve(0,piles,k);
     }
