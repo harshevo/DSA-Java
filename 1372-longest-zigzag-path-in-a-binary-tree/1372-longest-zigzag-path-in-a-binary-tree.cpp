f13c23439c878030ca754a7b1b268410856c81a2
Harsh Mishra <104376126+harshevo@users.noreply.github.com>
2023-04-19 18:59:01 +0530

    Time: 171 ms (66.73%), Space: 103.1 MB (33.45%) - LeetHub

--- 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.cpp
+++ 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.cpp
@@ -2,28 +2,41 @@
 class Solution {
 public:
     
-    int maxPath = 0;
-    void solve(TreeNode* root, int steps, bool pathLeft){
+//     int maxPath = 0;
+//     void solve(TreeNode* root, int steps, bool pathLeft){
+//         if(root == NULL){
+//             return;
+//         }
+        
+//         maxPath = max(maxPath,steps);
+        
+//         if(pathLeft){
+//             solve(root->left, steps+1, false); // pathleft set to false as we need to go right to form a zigzag pattern
+//             solve(root->right, 1, true);
+//         }
+//         else{
+//             solve(root->right, steps+1, true);
+//             solve(root->left, 1 , false);
+//         }
+        
+//     }
+    
+    //another code
+    int maxPath1 = 0;
+    void solve1(TreeNode* root, int l, int r){
         if(root == NULL){
             return;
         }
+        maxPath1 = max({maxPath1, l, r});
         
-        maxPath = max(maxPath,steps);
-        
-        if(pathLeft){
-            solve(root->left, steps+1, false); // pathleft set to false as we need to go right to form a zigzag pattern
-            solve(root->right, 1, true);
-        }
-        else{
-            solve(root->right, steps+1, true);
-            solve(root->left, 1 , false);
-        }
+        solve1(root->left, r+1 , 0);
+        solve1(root->right, 0, l+1);
         
     }
     
     int longestZigZag(TreeNode* root) {
-         solve(root, 0, true);
-         solve(root, 0, false);
-        return maxPath;
+         solve1(root, 0, 0);
+         
+        return maxPath1;
     }
 };
