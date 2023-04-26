249fa21d6321c79549d9e4361589014b1c439aff
Harsh Mishra <104376126+harshevo@users.noreply.github.com>
2023-04-26 14:12:56 +0530

    Time: 3 ms (56.14%), Space: 6 MB (63.10%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -3,22 +3,26 @@ public:
     
     int addDigits(int num) {
         
-        if(num < 10){
-            return num;
-        }
+//         if(num < 10){
+//             return num;
+//         }
         
-        int sum = 0;
+//         int sum = 0;
         
-        while(num != 0){
+//         while(num != 0){
             
-           sum = sum + num%10;
+//            sum = sum + num%10;
             
-            num = num/10;
+//             num = num/10;
             
-        }
+//         }
+        
+//        return addDigits(sum);
         
         
-       return addDigits(sum);
+        if (num == 0) return 0;
+        if (num % 9 == 0) return 9;
+        return num % 9;
         
     }
 };
