c3f748cafdf6ce82d6a4200efd62be34798ff671
Harsh Mishra <104376126+harshevo@users.noreply.github.com>
2023-04-26 14:14:11 +0530

    Time: 7 ms (16.24%), Space: 5.9 MB (63.10%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -19,10 +19,14 @@ public:
         
 //        return addDigits(sum);
         
+        // second solution
+//         if (num == 0) return 0;
+//         if (num % 9 == 0) return 9;
+//         return num % 9;
         
-        if (num == 0) return 0;
-        if (num % 9 == 0) return 9;
-        return num % 9;
         
+        //Third solution
+        
+        return num == 0 ? 0:1 + (num-1) %9;
     }
 };
